7305cb555109602762be60c01c7d512066dc49bd
Jake Kara <jake@jakekara.com>
2020-10-12 13:23:34 -0400

    fix race condition at terminal setup

--- src/NBPYProvider/kernelProvider.ts
+++ src/NBPYProvider/kernelProvider.ts
@@ -3,8 +3,8 @@ import { CancellationToken, NotebookCell, NotebookCommunication, NotebookDocumen
 
 export class NBPYKernelProvider implements NotebookKernelProvider<NBPYKernel>{
     // onDidChangeKernels?: Event<NotebookDocument | undefined>;
-    provideKernels(document: NotebookDocument, token: CancellationToken): ProviderResult<NBPYKernel[]>{
-        return new Promise((resolve, reject)=>{
+    provideKernels(document: NotebookDocument, token: CancellationToken): ProviderResult<NBPYKernel[]> {
+        return new Promise((resolve, reject) => {
             resolve([new NBPYKernel()]);
         });
     }
@@ -12,66 +12,71 @@ export class NBPYKernelProvider implements NotebookKernelProvider<NBPYKernel>{
 
 }
 
-export class NBPYKernel implements NotebookKernel{
+export class NBPYKernel implements NotebookKernel {
     label = "nbpy kernel";
 
     terminal?: vscode.Terminal;
 
-    constructor(){
+    constructor() {
+        const terminalOptions: vscode.TerminalOptions = {
+            name: "nbpy-session",
+        }
         vscode.commands.executeCommand("python.setInterpreter")
-        .then(()=>{
-        this.terminal= vscode.window.createTerminal("nbpy-session")
-        this.terminal.show()
-        // this.terminal.sendText("python --version")
-        this.terminal.sendText("python")
-        // this.terminal.sendText("import sys; sys.version")
+            .then(() => {
+                this.terminal = vscode.window.createTerminal(terminalOptions)
+                this.terminal.show()
+                // this.terminal.sendText("python --version")
 
-        });
+                setTimeout(() => this.terminal?.sendText("python"), 3000);
+
+                // this.terminal.sendText("import sys; sys.version")
+
+            });
 
     }
 
-    
+
     executeCell(document: NotebookDocument, cell: NotebookCell): Promise<void> {
 
         console.log("Executing cell", cell, cell.document.getText());
 
-        return new Promise((resolve, reject)=>{
-            if (cell.cellKind !== vscode.CellKind.Code){ 
+        return new Promise((resolve, reject) => {
+            if (cell.cellKind !== vscode.CellKind.Code) {
                 console.log("Skipping markdown cell", cell.document.getText())
                 resolve();
                 return;
             }
-            else if(!this.terminal){ 
-                reject("No terminal"); 
-                return; 
+            else if (!this.terminal) {
+                reject("No terminal");
+                return;
             }
-            else if(this.terminal){
+            else if (this.terminal) {
                 this.terminal.show()
                 this.terminal.sendText(cell.document.getText())
                 resolve();
             }
-    
+
 
         })
 
 
-        
-    }
-
-    cancelAllCellsExecution(){
 
     }
-     
-     executeAllCells(document: NotebookDocument): Promise<void> {
-         
+
+    cancelAllCellsExecution() {
+
+    }
+
+    executeAllCells(document: NotebookDocument): Promise<void> {
+
         console.log("Executing all cells")
-        return new Promise((resolve, reject)=>{
-            document.cells.forEach(c=>{this.executeCell(document, c)})
+        return new Promise((resolve, reject) => {
+            document.cells.forEach(c => { this.executeCell(document, c) })
         })
 
-     }
+    }
 
-     cancelCellExecution(){
+    cancelCellExecution() {
 
-     }
+    }
 }
